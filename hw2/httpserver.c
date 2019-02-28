a7e39950b92529dd232edd06aaa3ff1456e7e3af
Angie Wang <angiewang@berkeley.edu>
2019-02-28 06:22:27 +0000

    first try forst two functions

--- hw2/httpserver.c
+++ hw2/httpserver.c
@@ -51,9 +51,62 @@ void handle_files_request(int fd) {
    */
 
   struct http_request *request = http_request_parse(fd);
+  // get the final path the user requested.
+  char *dir_path = *server_files_directory;
+  char *path = malloc(strlen(dir_path) + strlen(request->path) + 1); 
+  strcpy(path, dir_path);
+  strcat(path, request->path);
+   
+  // check the given argument is a filename or a directory or 
+  FILE *file;
+  int is_file = 0;
+  if (file = fopen(filename, "r")){
+      fclose(file);
+      is_file = 1;
+  } // Problem: the prob is that derectory and nonexist file will all fall into is_file = 0 
+  
+  // 1) If user requested an existing file, respond with the file
+  // check whether exist 
+  // set content-type header
+  if(is_file){
+    char *ctheader = http_get_mime_type(request->path)
+    http_send_header(fd, "Content-Type", ctheader);
+    http_end_headers(fd);
+    http_start_response(fd, 200);
+    char read_buff[7810];
+    //open the file as fd
+    int sourcefd = open(path, O_RDONLY, 0666);
+    while(read(path, read_buff, sizeof(read_buff))){
+      http_send_data(sourcefd, read_buff, sizeof(read_buff));
+    }
+  }else{
+    //need to tell if it is the case that the file doesn't exist
+    char *ultpath = malloc(strlen(path) + strlen("/index.html") + 1);
+    if (file = fopen(filename, "r")){
+      http_send_header(fd, "Content-type", "text/html");
+      http_end_headers(fd);
+      http_start_response(fd, 200);
+    char read_buff[7810];
+    //open the file as fd
+    int sourcefd = open(ulypath, O_RDONLY, 0666);
+    while(read(ultpath, read_buff, sizeof(read_buff))){
+      http_send_data(sourcefd, read_buff, sizeof(read_buff));
+    }
+      fclose(file);
+    }else{
+      // file doesn't exist or directory doesn't contatin .html
+      // throw children and a link to the parent directory
+      
 
-  http_start_response(fd, 200);
-  http_send_header(fd, "Content-Type", "text/html");
+    }
+  }
+
+
+
+  
+  // set content-type header
+  char *ctheader = http_get_mime_type(request->path)
+  http_send_header(fd, "Content-Type", ctheader);
   http_end_headers(fd);
   http_send_string(fd,
       "<center>"
